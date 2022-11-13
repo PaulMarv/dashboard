18deb34c7f825f50f83359ed2ea233aa58f13cf3
PaulMarv <mpsalako@gmail.com>
2022-11-13 21:21:41 +0100

    new commit

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Logarithmic, Inject, HiloSeries, Tooltip, DateTime, Zoom, Logarithemetic, Crosshair} from '@syncfusion/ej2-react-charts';
+
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy';
+import {useStateContext} from '../../contexts/contextProvider';
+import {ChartsHeader} from '../../components';
+
+const date1 = new Date('2017, 1, 1');
+
+const filterValue = (value)=>{
+  if (value.x >= date1){
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue)
 
 const Financial = () => {
+  const { currentMode } = useStateContext()
+
   return (
-    <div>Financial</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary rounded-3xl'>
+      <ChartsHeader category="Financial" title="Apple Histotical "/>
+      <div className="w-full">
+        <ChartComponent
+          id='charts'
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{border:{width:0 }}}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line:{width:0}}}
+          background={ currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]}/>
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
