1fc06e7dc13832d236e6658bff84994dee4bf60b
PaulMarv <mpsalako@gmail.com>
2022-11-21 14:15:38 +0100

    new commit

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -9,39 +9,40 @@ const ColorMapping = () => {
   const { currentMode } = useStateContext();
 
   return (
-    <div className='m-4md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <ChartsHeader category = 'Color Mapping' title='USA CLIMATE - WEATHER BY MONTH'/>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Color Mappping" title="USA CLIMATE - WEATHER BY MONTH" />
       <div className="w-full">
         <ChartComponent
-          id = "charts"
+          id="charts"
           primaryXAxis={ColorMappingPrimaryXAxis}
           primaryYAxis={ColorMappingPrimaryYAxis}
-          chartArea={{ border: {width: 0}}}
-          legendSettings={{ mode: 'Range', background: 'white'}}
-          tooltip={{ enable: true}}
-          background={currentMode === 'Dark'? '#33373E' : '#fff'}
+          chartArea={{ border: { width: 0 } }}
+          legendSettings={{ mode: 'Range', background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
         >
-          <Inject services={[ColumnSeries, Tooltip, Category, Legend]}/>
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
           <SeriesCollectionDirective>
             <SeriesDirective
               dataSource={colorMappingData[0]}
-              name='USA'
-              xName='x'
-              yName='y'
-              type='column'
+              name="USA"
+              xName="x"
+              yName="y"
+              type="Column"
               cornerRadius={{
                 topLeft: 10,
-                topRight:10
+                topRight: 10,
               }}
             />
           </SeriesCollectionDirective>
           <RangeColorSettingsDirective>
-            {rangeColorMapping.map((item,index) => <RangeColorSettingDirective key={index} {...item}/>)}
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item} />)}
           </RangeColorSettingsDirective>
         </ChartComponent>
       </div>
     </div>
-  )
+  );
 };
 
-export default ColorMapping
+export default ColorMapping;
